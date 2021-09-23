191ec55a5ba4c86693e7fc4aaae6abdb5d0796a4
poswords <poswords@gmail.com>
2021-09-23 15:25:04 +0900

    Update worldFlipper.js

--- commands/worldFlipper.js
+++ commands/worldFlipper.js
@@ -14,6 +14,10 @@ const soulReaction = '📀';
 const numberReactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
 const prefix = process.env.PREFIX || '!!';
 
+const catchErr = err => {
+  console.log(err)
+}
+
 const getInfoEmbed = (unit, flag) => {
   var footer = unit.Stance + ' - ' + unit.Role + ' - ' + unit.Gender + ' - ' + unit.Race;
   const rarity = Array(parseInt(unit.Rarity, 10)).fill(':star:').join('');
@@ -159,7 +163,7 @@ const appendReacts = async (units, message, type, msg, max=10) => {
   let num = units.length;
   if (units.length > max) num = max;
   for (let i = 0; i < num; i++) {
-    await msg.react(numberReactions[i]);
+    await msg.react(numberReactions[i]).catch(catchErr);
   }
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
@@ -185,11 +189,12 @@ const appendReacts = async (units, message, type, msg, max=10) => {
             sendTitle(units[i], message);
             break;
         }
-        msg.reactions.removeAll();
+        msg.reactions.removeAll().catch(catchErr)
+        ;
       }
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 }
 
 const sendMessage = async (unit, message) => {
@@ -197,8 +202,8 @@ const sendMessage = async (unit, message) => {
     return [normalReaction, awakenReaction].includes(reaction.emoji.name) && user.id === message.author.id;
   };
   const msg = await message.channel.send({embeds:[getInfoEmbed(unit, 'normal')]});
-  await msg.react(normalReaction);
-  await msg.react(awakenReaction);
+  await msg.react(normalReaction).catch(catchErr);
+  await msg.react(awakenReaction).catch(catchErr);
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
     if (r.emoji.name === normalReaction) {
@@ -208,7 +213,7 @@ const sendMessage = async (unit, message) => {
       msg.edit({embeds:[getInfoEmbed(unit, 'awaken')]});
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 };
 
 const sendEquip = async (unit, message) => {
@@ -216,8 +221,8 @@ const sendEquip = async (unit, message) => {
     return [weaponReaction, soulReaction].includes(reaction.emoji.name) && user.id === message.author.id;
   };
   const msg = await message.channel.send({embeds:[getEquipEmbed(unit, 'icon')]});
-  await msg.react(weaponReaction);
-  await msg.react(soulReaction);
+  await msg.react(weaponReaction).catch(catchErr);
+  await msg.react(soulReaction).catch(catchErr);
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
     if (r.emoji.name === weaponReaction) {
@@ -227,7 +232,7 @@ const sendEquip = async (unit, message) => {
       msg.edit({embeds:[getEquipEmbed(unit, 'soul')]});
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 };
 
 const sendThumbnail = async (unit, message) => {
@@ -235,8 +240,8 @@ const sendThumbnail = async (unit, message) => {
     return [normalReaction, awakenReaction].includes(reaction.emoji.name) && user.id === message.author.id;
   };
   const msg = await message.channel.send({embeds:[getThumbnailEmbed(unit, 'normal')]});
-  await msg.react(normalReaction);
-  await msg.react(awakenReaction);
+  await msg.react(normalReaction).catch(catchErr);
+  await msg.react(awakenReaction).catch(catchErr);
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
     if (r.emoji.name === normalReaction) {
@@ -246,7 +251,7 @@ const sendThumbnail = async (unit, message) => {
       msg.edit({embeds:[getThumbnailEmbed(unit, 'awaken')]});
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 };
 
 const sendArt = async (unit, message) => {
@@ -254,8 +259,8 @@ const sendArt = async (unit, message) => {
     return [normalReaction, awakenReaction].includes(reaction.emoji.name) && user.id === message.author.id;
   };
   const msg = await message.channel.send({embeds:[getArtEmbed(unit, 'normal')]});
-  await msg.react(normalReaction);
-  await msg.react(awakenReaction);
+  await msg.react(normalReaction).catch(catchErr);
+  await msg.react(awakenReaction).catch(catchErr);
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
     if (r.emoji.name === normalReaction) {
@@ -265,7 +270,7 @@ const sendArt = async (unit, message) => {
       msg.edit({embeds:[getArtEmbed(unit, 'awaken')]});
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 };
 
 const sendAlt = async (unit, message) => {
@@ -273,8 +278,8 @@ const sendAlt = async (unit, message) => {
     return [normalReaction, awakenReaction].includes(reaction.emoji.name) && user.id === message.author.id;
   };
   const msg = await message.channel.send({embeds:[getArtEmbed(unit, 'awaken')]});
-  await msg.react(normalReaction);
-  await msg.react(awakenReaction);
+  await msg.react(normalReaction).catch(catchErr);
+  await msg.react(awakenReaction).catch(catchErr);
   const collector = msg.createReactionCollector({ filter, time: reactionExpiry});
   collector.on('collect', r => {
     if (r.emoji.name === normalReaction) {
@@ -284,7 +289,7 @@ const sendAlt = async (unit, message) => {
       msg.edit({embeds:[getArtEmbed(unit, 'awaken')]});
     }
   });
-  collector.on('end', collected => msg.reactions.removeAll());
+  collector.on('end', collected => msg.reactions.removeAll().catch(catchErr));
 };
 
 const sendTitle = async (unit, message) => {

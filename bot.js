9322eeaccc9b4f0951854112f29f4d30075e3d70
poswords <poswords@gmail.com>
2022-10-05 00:08:35 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,10 +20,40 @@ for (const file of commandFiles) {
     client.commands.set(commands.name, commands);
   }
 }
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    /*console.log(client.guilds.cache.map(guild => guild.name + " " + guild.id).join("\n"));
     
+    var guilds = client.guilds.cache.map(guild => guild.id);
+    /*
+    shuffle(guilds);
+    for (i =1; i<=250; i++){
+      const guild =client.guilds.cache.find(guild => guild.id = guilds[i]);
+      if (guilds[i]!='646623094269673492'){
+        guild.leave()
+        .then(g => console.log(`left ${g}`))
+        .catch(console.error);
+      }
+
+    }*/
+    
+    /*
     const guild = client.guilds.cache.find(guild => guild.name == "DaddyF2P's Kindergarten");
     guild.leave()
       .then(g => console.log(`left ${g}`))
